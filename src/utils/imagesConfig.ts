f686fb2dfe86a1115e188fec8b4bcacb24bce6e9
progali007 <code.progali@gmail.com>
2023-05-07 18:15:46 +0430

    :fire: Remove imageConfig file

--- src/utils/imagesConfig.ts
+++
@@ -1,9 +0,0 @@
-import fileDefault from "../assets/images/file-blank-solid-240.png";
-import filePdf from "../assets/images/file-pdf-solid-240.png";
-import filePng from "../assets/images/file-png-solid-240.png";
-
-export const imagesConfig: {[key:string]: string} = {
-  "image/png": filePng,
-  "application/pdf": filePdf,
-  "text/plain": fileDefault,
-}
