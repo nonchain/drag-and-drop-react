2e92c23a7ba662974c9c4f2ffa37012ee6f2bf78
progali007 <code.progali@gmail.com>
2023-05-07 12:36:04 +0430

    :sparkles: (file-item): Add image to show the uploaded image

--- src/components/file/file-item.component.tsx
+++ src/components/file/file-item.component.tsx
@@ -1,25 +1,43 @@
 // Components
-import { Stack, Image, Text } from "@chakra-ui/react"
+import { Stack, Image, Text } from "@chakra-ui/react";
 // Files
 import { imagesConfig } from "../../utils/imagesConfig";
 // TS configs
 import { FileItemProps } from "../../models/types";
 
-function FileItem({file}: FileItemProps) {
-
-  if(!file) return null;
+function FileItem({ file, base64 }: FileItemProps) {
+  if (!file.name) return null;
 
   const { name, size, type } = file;
-  const formattedSize = Intl.NumberFormat('en', { notation: 'compact' }).format(Number(size));
+  const formattedSize = Intl.NumberFormat("en", { notation: "compact" }).format(
+    Number(size)
+  );
+
+  const imgSrc = file.type === "image/png" ? base64 : "";
   return (
-    <Stack direction="row">
-      <Image src={imagesConfig[type]} alt="icon" width="3rem"/>
-      <Stack justifyContent="center">
-        <Text fontSize="0.812rem" fontWeight="600">{name}</Text>
-        <Text color="#585859" fontSize="0.65rem" fontWeight="500" sx={{"&.chakra-text": {margin: 0}}}>{formattedSize}B</Text>
+    <Stack gap="1rem">
+      <Stack direction="row">
+        <Image src={imagesConfig[type]} alt="icon" width="3rem" />
+        <Stack justifyContent="center">
+          <Text fontSize="0.812rem" fontWeight="600">
+            {name}
+          </Text>
+          <Text
+            color="#585859"
+            fontSize="0.65rem"
+            fontWeight="500"
+            sx={{ "&.chakra-text": { margin: 0 } }}
+          >
+            {formattedSize}B
+          </Text>
+        </Stack>
       </Stack>
+
+      {
+        imgSrc ? <Image height="12rem" src={imgSrc} alt="image" /> : null
+      }
     </Stack>
-  )
+  );
 }
 
-export default FileItem
+export default FileItem;
