df1e982961e4bb783d8b2e8d5ddcf187ec7972b5
progali007 <code.progali@gmail.com>
2023-05-07 11:50:27 +0430

    :sparkles: (imageConfig): Define types

--- src/utils/imagesConfig.ts
+++ src/utils/imagesConfig.ts
@@ -2,7 +2,7 @@ import fileDefault from "../assets/images/file-blank-solid-240.png";
 import filePdf from "../assets/images/file-pdf-solid-240.png";
 import filePng from "../assets/images/file-png-solid-240.png";
 
-export const imagesConfig = {
+export const imagesConfig: {[key:string]: string} = {
   "image/png": filePng,
   "application/pdf": filePdf,
   "text/plain": fileDefault,
