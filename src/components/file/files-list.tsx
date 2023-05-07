f7255d473e4742d0f7dee87baeed930a2a3d348c
progali007 <code.progali@gmail.com>
2023-05-07 22:23:02 +0430

    :sparkles: (file-list): Build list component for uploaded files

---
+++ src/components/file/files-list.tsx
@@ -0,0 +1,19 @@
+import { Stack } from "@chakra-ui/react";
+import FileItem from "./file-item.component";
+// TS Config
+import { FilesListProps } from "../../models/types";
+
+function FilesList({ filesList }: FilesListProps) {
+
+  if(filesList?.length < 0) return null;
+
+  return (
+    <Stack gap="0.5rem">
+      {
+        filesList?.map(file => <FileItem file={file.file} base64={file.base64} />)
+      }
+    </Stack>
+  );
+}
+
+export default FilesList;
