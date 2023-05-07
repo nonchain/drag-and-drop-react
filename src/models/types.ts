924df8e0c7397d7826529c1fa559294ab35492d3
progali007 <code.progali@gmail.com>
2023-05-07 22:28:37 +0430

    :wrench: (types): Add functions to types

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,5 +1,9 @@
-export type File = { name: string; type: string; size: string; };
-export type FileInput = { name: string; type: string; size: string; file: object; };
+export type File = { id: string; name: string; type: string; size: string; };
+export type FileInput = { id:string; name: string; type: string; size: string; file: object; };
+export type DraggedFile = {
+  file: File;
+  base64: string;
+};
 
 export type FileInputProps = {
   onChange: (file: FileInput) => void;
@@ -8,6 +12,10 @@ export type FileInputProps = {
 export type FileItemProps = {
   file: File;
   base64: string;
+  onDeleteFile: (fileId: string) => void;
 };
 
-
+export type FilesListProps = {
+  filesList: DraggedFile[]
+  onDeleteFile: (fileId: string) => void;
+}
