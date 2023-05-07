3a38ed6ed3747c098ec957dcdc3edef3a92fc5ce
progali007 <code.progali@gmail.com>
2023-05-07 11:24:58 +0430

    :sparkles: (App): Setup Toaster component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import './App.css'
+// Libraries
+import { Fragment } from "react";
+import { Toaster } from "react-hot-toast";
+// Components
+import DragDropFile from "./components/drag-and-drop/drag-drop-file";
+import { Stack } from "@chakra-ui/react";
 
 function App() {
   return (
-    <h1>Starter file</h1>
-  )
+    <Fragment>
+      <Toaster toastOptions={{
+        duration: 3500,
+        style: {
+          fontSize: "0.912rem",
+          fontWeight: "600"
+        },
+      }}/>
+      <Stack minHeight="100vh" bgColor="#FEFAFA" alignItems="center" justifyContent="center">
+        <DragDropFile />
+      </Stack>
+    </Fragment>
+  );
 }
 
-export default App
+export default App;
